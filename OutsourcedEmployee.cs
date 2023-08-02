f477f76c6546b870f68e2579d6837ba5db81ae64
pmerlin04 <92960530+pmerlin04@users.noreply.github.com>
2023-08-02 15:58:08 -0300

    Add files via upload

---
+++ OutsourcedEmployee.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ExercicioHerancaPolimorfismo.Entities
+{
+    //subclasse da Employee
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) :
+            base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        //a palavra override significa que este método está sendo subescrevido
+        public override double Payment()
+        {
+            return base.Payment() + (AdditionalCharge * (110 / 100));
+        }
+    }
+}
